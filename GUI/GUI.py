460e9b2aff110dc2c2006371b2670a827f5294a5
jjinhub <khj05592002@naver.com>
2025-02-20 21:53:13 +0900

    프로그램 실행

---
+++ GUI/GUI.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt5.QtWidgets import *
+
+class MainWindow(QMainWindow):
+    """
+    Main Window 클래스
+    """
+    def __init__(self):
+        """
+        QMainWindow를 상속받아 Main Window를 생성
+        """
+        super().__init__()
+
+if __name__ == "__main__":
+
+    # 프로그램 실행 클래스
+    app = QApplication(sys.argv)
+
+    # MainWindow 클래스 인스턴스 생성
+    main = MainWindow()
+
+    # Main Window UI를 보여주는 메서드 실행
+    main.show()
+
+    # 프로그램 실행
+    app.exec_()

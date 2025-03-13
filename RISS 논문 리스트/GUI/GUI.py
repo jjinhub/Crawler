4e51bae3272696e1dffbf95f2ed351cd41f1f615
jjinhub <khj05592002@naver.com>
2025-03-14 02:07:35 +0900

    메뉴바 생성

--- RISS 논문 리스트/GUI/GUI.py
+++ RISS 논문 리스트/GUI/GUI.py
@@ -19,10 +19,22 @@ class MainWindow(QMainWindow):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+    def menu_bar(self):
+
+        # menuBar 메서드를 통해 QMenuBar 클래스의 인스턴스 생성
+        menubar = self.menuBar()
+
+        # addMenu 메서드를 사용하여 Setting라는 이름을 가진 QMenu 클래스의 인스턴스를 생성
+        setting_menu = menubar.addMenu("Setting")
+
+        # addAction 메서드를 사용하여 Option Menu가 클릭되었을 때, Start라는 텍스트의 QAction 인스턴스를 생성
+        # start_action = option_menu.addAction("Start")
+
     def initUI(self):
         self.setWindowTitle("크롤러 GUI") # GUI 타이틀
         self.resize(1600, 900) # GUI 크기
         self.center() # 화면 가운데 정렬
+        self.menu_bar() # 메뉴바 생성성
 
 if __name__ == "__main__":
 
